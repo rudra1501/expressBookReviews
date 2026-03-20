f6f548955838635d8b304b325befef8e92e30e0a
rudra1501 <rudramahyavanshi15@gmail.com>
2026-03-20 23:03:08 +0530

    task 6- registered user

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,21 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password required" });
+  }
+
+  if (isValid(username)) {
+    return res.status(409).json({ message: "User already exists" });
+  }
+
+  users.push({ username, password });
+
+  return res.status(200).json({
+    message: "User successfully registered. Now you can login"
+  });
 });
 
 // Get the book list available in the shop
