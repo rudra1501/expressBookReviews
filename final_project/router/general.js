807bb9c0ac602a9a06f9ae560e1125af54612a32
rudra1501 <rudramahyavanshi15@gmail.com>
2026-03-20 22:38:36 +0530

    task 3-  get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,8 +30,20 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+
+  const filteredBooks = Object.keys(books)
+    .filter(key => books[key].author === author)
+    .reduce((result, key) => {
+      result[key] = books[key];
+      return result;
+    }, {});
+
+  if (Object.keys(filteredBooks).length === 0) {
+    return res.status(404).json({ message: "No books found for this author" });
+  }
+
+  return res.status(200).json(filteredBooks);
 });
 
 // Get all books based on title
