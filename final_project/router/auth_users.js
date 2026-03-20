07a327ee64c97541e055c64f21db2cb701c86ff0
rudra1501 <rudramahyavanshi15@gmail.com>
2026-03-20 23:45:19 +0530

    review implemented

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -47,8 +47,33 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+  const username = req.session.authorization.username;
+  const isbn = req.params.isbn;
+  const { review } = req.body;
+
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+
+  if (!review) {
+    if (!book.reviews[username]) {
+      return res.status(404).json({ message: "No review found to delete" });
+    }
+
+    delete book.reviews[username];
+
+    return res.status(200).json({ message: "Review deleted successfully" });
+  }
+
+  book.reviews[username] = review;
+
+  return res.status(200).json({
+    message: "Review added/updated successfully",
+    reviews: book.reviews
+  });
 });
 
 module.exports.authenticated = regd_users;
