cc1710fcfe51ce4d65178507de6d7e3b0d130e0a
rudra1501 <rudramahyavanshi15@gmail.com>
2026-03-20 22:42:05 +0530

    task 4- get books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,8 +48,20 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+
+  const filteredBooks = Object.keys(books)
+    .filter(key => books[key].title === title)
+    .reduce((result, key) => {
+      result[key] = books[key];
+      return result;
+    }, {});
+
+  if (Object.keys(filteredBooks).length === 0) {
+    return res.status(404).json({ message: "No books found for this title" });
+  }
+
+  return res.status(200).json(filteredBooks);
 });
 
 //  Get book review
